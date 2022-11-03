528442a4c24708f2d285f3d39284a5652329e3d6
JPBM135 <jpedrobm0@gmail.com>
2022-11-02 23:19:01 -0300

    Revert "init"
    This reverts commit bf81f70856856f081e2e467f7653bf54ffd26a5e.

--- src/interactions/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './create.js';
-export * from './edit.js';
-export * from './settings.js';
-export * from './lookup.js';
--- src/utils/validadeCode.ts
+++
@@ -1,5 +0,0 @@
-const regex = /^[A-Z]{2}\d{9}[A-Z]{2}$/;
-
-export function validateCode(code: string): boolean {
-	return regex.test(code);
-}
